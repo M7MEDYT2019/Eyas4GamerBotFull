354ae604fa12f6d18975e0c3daa6a0f22c900f22
M7MEDYT2019 <42390271+M7MEDYT2019@users.noreply.github.com>
2018-08-18 02:42:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1338,34 +1338,37 @@ client.on('message', message => {
 
 
 
+const Discord = require("discord.js");
+const botconfig = require("../botconfig");
+let purple = botconfig.purple;
+let xp = require("../xp.json");
+
 module.exports.run = async (bot, message, args) => {
-    message.delete();
-    if(args[0] == "help"){
-      message.reply("Usage: !report <user> <reason>");
-      return;
-    }
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return errors.cantfindUser(message.channel);
-    let rreason = args.join(" ").slice(22);
-    if(!rreason) return errors.noReason(message.channel);
 
-    let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Reports")
-    .setColor(orange)
-    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-    .addField("Channel", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", rreason);
+  if(!xp[message.author.id]){
+   xp[message.author.id] = {
+     xp: 0,
+     level: 1
+  };
+}
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 300;
+  let difference = nxtLvlXp - curxp;
 
-    let reportschannel = message.guild.channels.find(`name`, "reports");
-    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-    reportschannel.send(reportEmbed);
+  let lvlEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor(Red)
+  .addField("Level", curlvl, true)
+  .addField("XP", curxp, true)
+  .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
+
+  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
 
 }
 
 module.exports.help = {
-  name: "report"
+  name: "level"
 }
 
 
